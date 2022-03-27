852851513901bfd7b532cc79c0f7b8310c427d24
Michael Pollind <mpollind@gmail.com>
2022-03-26 19:42:20 -0700

    update name

--- generator/src/main.rs
+++ generator/src/main.rs
@@ -12,8 +12,8 @@ use std::os::raw;
 fn main() {
     let cwd = std::env::current_dir().unwrap();
     if cwd.ends_with("generator") {
-        write_source_code(Path::new("OpenGL-Registry"), "../glew-rs/src");
+        write_source_code(Path::new("OpenGL-Registry"), "../rogl/src");
     } else {
-        write_source_code(Path::new("generator/OpenGL-Registry"), "./glew-rs/src");
+        write_source_code(Path::new("generator/OpenGL-Registry"), "./rogl/src");
     }
 }
